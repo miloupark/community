f68903e9555c52a6ef1dc25c298d686e1d3898ef
miloupark <136055426+miloupark@users.noreply.github.com>
2025-04-09 23:50:48 +0900

    feat: add basic component with state and styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,31 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, TextInput, Button } from 'react-native';
 
 export default function HomeScreen() {
+
+  const [text, setText] = useState('')
+
   return (
-    <View></View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>HI</Text>
+      </View>
+      <TextInput value={text} onChangeText={(value)=> setText(value)}
+        style={styles.input}/>
+        <Button title="button title" onPress={()=>console.log('pressed!')}/>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 30,
+    color: "white",
+  },
+  input: {
+    fontSize: 30,
+  }
+});
