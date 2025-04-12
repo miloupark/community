8d86f17c924f5ca5b9450ba4a8d3ca8b6c40d0e8
miloupark <136055426+miloupark@users.noreply.github.com>
2025-04-12 23:16:05 +0900

    docs: learn how to add tab icons using Ionicons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
-import { colors } from '@/constants';
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { colors } from "@/constants";
+import { Tabs } from "expo-router";
+import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
   return (
@@ -13,19 +14,22 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: '홈',
+          title: "홈",
+          tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "home-sharp" : "home-outline"} size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
-          title: '내 프로필',
+          title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "person-circle-sharp" : "person-circle-outline"} size={25} color={color} />,
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
-          title: '설정',
+          title: "설정",
+          tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "settings" : "settings-outline"} size={25} color={color} />,
         }}
       />
     </Tabs>
