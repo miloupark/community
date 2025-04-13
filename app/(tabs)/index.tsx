946b6c8d9650c18e9770e431fce7844d2c98c7b3
miloupark <136055426+miloupark@users.noreply.github.com>
2025-04-13 18:07:11 +0900

    docs: practice creating a common button component using props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import { Text, SafeAreaView } from 'react-native';
+import CustomButton from "@/components/CustomButton";
+import { Text, SafeAreaView } from "react-native";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>홈스크린</Text>
+      <CustomButton label="버튼" onPress={() => {}} />
     </SafeAreaView>
   );
 }
