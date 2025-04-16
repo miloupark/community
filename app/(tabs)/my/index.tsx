5b8559eb7347bc4ccc0b1f47ea7545471c28a253
miloupark <136055426+miloupark@users.noreply.github.com>
2025-04-17 00:03:01 +0900

    docs: add signup form input handling and validation

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -1,6 +1,11 @@
-import { Text, SafeAreaView } from 'react-native';
+import { router, useFocusEffect } from "expo-router";
+import { Text, SafeAreaView } from "react-native";
 
 export default function MyScreen() {
+  useFocusEffect(() => {
+    router.replace("/auth");
+  });
+
   return (
     <SafeAreaView>
       <Text>내정보 홈스크린</Text>
--- components/inputField.tsx
+++ components/inputField.tsx
@@ -11,19 +11,32 @@ import { colors } from "@/constants";
 interface inputFieldProps extends TextInputProps {
   label?: string;
   variant?: "filled" | "standard" | "outlined";
+  error?: string;
 }
 
-function inputField({ label, variant = "filled", ...props }: inputFieldProps) {
+function inputField({
+  label,
+  variant = "filled",
+  error = "",
+  ...props
+}: inputFieldProps) {
   return (
     <View>
       {label && <Text style={styles.label}>{label}</Text>}
-      <View style={[styles.container, styles[variant]]}>
+      <View
+        style={[
+          styles.container,
+          styles[variant],
+          Boolean(error) && styles.inputError,
+        ]}
+      >
         <TextInput
           placeholderTextColor={colors.GRAY_500}
           style={styles.input}
           {...props}
         />
       </View>
+      {Boolean(error) && <Text style={styles.error}>{error}</Text>}
     </View>
   );
 }
@@ -34,7 +47,8 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingHorizontal: 10,
     justifyContent: "center",
-    // alignItems: "center",
+    alignItems: "center",
+    flexDirection: "row",
   },
   label: {
     fontSize: 12,
@@ -50,6 +64,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding: 0,
     flex: 1,
+  },
+  error: {
+    fontSize: 12,
+    marginTop: 5,
+    color: colors.RED_500,
+  },
+  inputError: {
+    backgroundColor: colors.RED_100,
   },
 });
 
