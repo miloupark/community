e18406505eb16d2f6d94d158519e717b629d9128
miloupark <136055426+miloupark@users.noreply.github.com>
2025-04-20 21:36:21 +0900

    docs: add validation using React Hook Form

--- components/EmailInput.tsx
+++ components/EmailInput.tsx
@@ -8,12 +8,23 @@ const EmailInput = () => {
     <Controller
       name="email"
       control={control}
-      render={({ field: { onChange, value } }) => (
+      rules={{
+        validate: (data: string) => {
+          if (data.length === 0) {
+            return "이메일을 입력해주세요.";
+          }
+          if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data)) {
+            return "올바른 이메일 형식이 아닙니다.";
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
           label="이메일"
           placeholder="이메일을 입력해주세요."
           value={value}
           onChangeText={onChange}
+          error={error?.message}
         />
       )}
     />
--- components/PasswordConfirm.tsx
+++ components/PasswordConfirm.tsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Controller, useFormContext } from "react-hook-form";
+import { Controller, useFormContext, useWatch } from "react-hook-form";
 import InputField from "@/components/inputField";
 
 const PasswordConfirm = () => {
   const { control } = useFormContext();
+  const password = useWatch({ control, name: "password" });
   return (
     <Controller
       name="passwordConfirm"
       control={control}
-      render={({ field: { onChange, value } }) => (
+      rules={{
+        validate: (data: string) => {
+          if (data !== password) {
+            return "비밀번호가 일치하지 않습니다.";
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
           label="비밀번호 확인"
           placeholder="비밀번호를 입력해주세요."
+          secureTextEntry
           value={value}
           onChangeText={onChange}
+          error={error?.message}
         />
       )}
     />
